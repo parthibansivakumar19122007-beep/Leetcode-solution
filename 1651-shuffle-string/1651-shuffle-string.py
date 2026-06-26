47ab4e6f61206da6aac8b65f1614fb62e7dc48ec
parthibansivakumar19122007-beep <parthibansivakumar19122007@gmail.com>
2026-06-26 22:19:09 +0530

    Time: 0 ms (100%), Space: 12.5 MB (19.11%) - LeetHub

---
+++ 1651-shuffle-string/1651-shuffle-string.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def restoreString(self, s, indices):
+        result=['']*len(s)
+        for i in range(len(s)):
+            result[indices[i]]=s[i]
+        return ''.join(result)
