201cc8c2df7f58a9eadb377e66134e0a192e664c
parthibansivakumar19122007-beep <parthibansivakumar19122007@gmail.com>
2026-08-03 19:11:21 +0530

    Time: 7 ms (54.4%), Space: 13.2 MB (57.72%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,9 +1,10 @@
 class Solution(object):
     def pivotIndex(self, nums):
-        t=sum(nums)
         l=0
+        t=sum(nums)
+        c=0
         for i,num in enumerate(nums):
-            if l==(t-l-num):
+            if l==t-l-num:
                 return i
             l+=num
         return -1
